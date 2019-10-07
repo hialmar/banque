41e6070bea05304b688fac7e67c07fddc1523fe0
Patrice Torguet <torguet@gmail.com>
2019-10-07 14:21:13 +0200

    Généralisation lien vers appli nodeJS

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -13,7 +13,7 @@ angular
     'ngResource'
   ])
   .factory('MyFactoryCompte', function ($resource) {
-     return $resource('https://hialmar-banque.herokuapp.com/compte/:id', { id: '@_id' }, {
+     return $resource('/compte/:id', { id: '@_id' }, {
         update: {
           method: 'PUT' // this method issues a PUT request
         }
