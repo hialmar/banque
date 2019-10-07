dcc95b18c574cc5e2b5070c758154fc1a3226a10
Patrice Torguet <torguet@gmail.com>
2019-10-07 13:43:35 +0200

    Correction petits problèmes.

---
+++ app.js
@@ -0,0 +1,96 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var banque = require('./banque');
+
+function isInt(value) {
+  return !isNaN(value) &&
+         parseInt(Number(value)) == value &&
+         !isNaN(parseInt(value, 10));
+}
+
+/*
+// Si on lance l'application Angular.js avec grunt serve
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    next();
+});
+*/
+// Si on veut que l'application Angular.js soit envoyé par notre application Node.js
+app.use(express.static(__dirname + '/public'));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
+
+app.use(bodyParser.json()); // pour parser du JSON
+
+app.post('/compte/', function (req, res) {
+        console.log(req.body);
+        if(typeof req.body.id === 'undefined' || ! isInt(req.body.somme)) {
+          res.status(400).json({ error: 'Il faut préciser les paramètres.' });
+        } else if(banque.creerCompte(req.body.id,parseInt(req.body.somme))) {
+                var pos = banque.positionDuCompte(req.body.id);
+                if(pos) {
+                        console.log(pos);
+                        res.json(pos);
+                } else {
+                        res.status(404).json({ error: "Le compte d'id "+req.params.id+" n'existe pas." });
+                }
+        } else {
+                res.status(409).json({ error: "Le compte d'id "+req.body.id+" existe déjà." });
+        }
+});
+
+app.get('/compte/:id', function(req, res) {
+
+        console.log(req.query);
+        if(typeof req.params.id === 'undefined') {
+                res.status(400).json({ error: 'Il faut préciser les paramètres.' });
+        } else {
+                var pos = banque.positionDuCompte(req.params.id);
+                if(pos) {
+                        console.log(pos);
+                        res.json(pos);
+                } else {
+                        res.status(404).json({ error: "Le compte d'id "+req.params.id+" n'existe pas." });
+                }
+        }
+});
+
+app.put('/compte/:id', function(req, res) {
+        console.log(req.query);
+        var pos;
+        if(typeof req.params.id === 'undefined' ||  ! isInt(req.body.somme)) {
+                res.status(400).json({ error: 'Il faut préciser les paramètres.' });
+        } else {
+                if(req.body.somme > 0) {
+                        if(banque.ajouterAuCompte(req.body.id,parseInt(req.body.somme))) {
+                              pos = banque.positionDuCompte(req.params.id);
+                              if(pos) {
+                                      console.log(pos);
+                                      res.json(pos);
+                              } else {
+                                      res.status(404).json({ error: "Le compte d'id "+req.params.id+" n'existe pas." });
+                              }
+                        } else {
+                              res.status(404).json({ error: "Le compte d'id "+req.params.id+" n'existe pas." });
+                        }
+                } else {
+                        if(banque.retirerDuCompte(req.body.id, - parseInt(req.body.somme))) {
+                              pos = banque.positionDuCompte(req.params.id);
+                              if(pos) {
+                                      console.log(pos);
+                                      res.json(pos);
+                              } else {
+                                      res.status(404).json({ error: "Le compte d'id "+req.params.id+" n'existe pas." });
+                              }
+                        } else {
+                              res.status(404).json({ error: "Le compte d'id "+req.params.id+" n'existe pas." });
+                        }
+                }
+        }
+});
+
+app.listen(3000, function () {
+console.log('Example app listening on port 3000!') ;
+}) ;
---
+++ public/scripts/app.js
@@ -0,0 +1,67 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name tp2App
+ * @description
+ * # tp2App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('tp2App', [
+    'ngResource'
+  ])
+  .factory('MyFactoryCompte', function ($resource) {
+     return $resource('http://localhost:3000/compte/:id', { id: '@_id' }, {
+        update: {
+          method: 'PUT' // this method issues a PUT request
+        }
+      });
+  })
+  .controller('MainCtrl',
+  function ($scope, MyFactoryCompte) {
+    $scope.resultat = '';
+    $scope.obj = { id : '0', somme : '0' };
+    $scope.creerCompte = function() {
+      var objToSave = new MyFactoryCompte();
+      objToSave.id = $scope.obj.id;
+      objToSave.somme = $scope.obj.somme;
+      objToSave.$save(function(savedObj) {
+        $scope.resultat = savedObj;
+       }, function(error) {
+        $scope.resultat = error.data.error;
+       });
+    };
+
+    $scope.position = function() {
+      MyFactoryCompte.get({ id: $scope.obj.id }, function(retour) {
+        $scope.resultat = retour;
+      }, function(error) {
+        $scope.resultat = error.data.error;
+       });
+    };
+
+    $scope.crediter = function() {
+      //alert('test credit');
+      MyFactoryCompte.update({ id:$scope.obj.id }, $scope.obj, function(savedObj) {
+        $scope.resultat = savedObj;
+       }, function(error) {
+        $scope.resultat = error.data.error;
+       });
+    };
+
+    $scope.debiter = function() {
+      //alert('test débit');
+      var obj = { id : $scope.obj.id, somme : $scope.obj.somme };
+
+      if(obj.somme > 0) obj.somme = - obj.somme;
+      MyFactoryCompte.update({ id:obj.id }, obj, function(savedObj) {
+        $scope.resultat = savedObj;
+       }, function(error) {
+        $scope.resultat = error.data.error;
+       });
+    };
+
+  });
+
